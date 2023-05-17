dffd5083d401cf6d1267ac3eb3aedd530fdc3c9d
Jyotsna Jha <jyotsna.jha003@gmail.com>
2023-05-17 09:54:07 +0545

    second commit

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,25 @@
+import pro1 from "../assets/figma1.jpeg";
+import pro2 from "../assets/figma2.png";
+
+
+const ProjectCardData=[
+    {
+        imgsrc:pro1,
+        title:"ToDo List UI design",
+        text:"",
+        view:"",
+    }
+,
+{
+    imgsrc:pro1,
+        title:"Food App UI design",
+        text:"",
+        view:"",
+},
+
+
+
+
+];
+
+export default ProjectCardData;
